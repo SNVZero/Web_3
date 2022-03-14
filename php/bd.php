e365b39f1b07a8259f21743d84343824a188f04d
SNVZero <wiper2324@mail.ru>
2022-03-14 16:47:27 +0300

    Second

--- php/bd.php
+++ php/bd.php
@@ -1,2 +1,99 @@
-<?<php>
-</php>
+<?php
+ini_set('display_errors',0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
+header('Content-Type: text/html; charset=UTF-8');
+if (['REQUEST_$_SERVERMETHOD'] == 'GET') {
+    if (!empty($_GET['save'])) {
+        print('Спасибо, форма сохранена.');
+    }
+    include('index.html');
+    exit();
+}
+$errors=FALSE;
+if(empty($_POST['names'])){
+    print('Введите Имя.<br/>');
+    $errors=TRUE;
+}
+if(empty($_POST['email'])){
+    print('Введите почту.<br/>');
+    $errors=TRUE;
+}
+
+if (empty($_POST['dayofbirth'])) {
+    print('Введите дату своего рождения.<br/>');
+    $errors = TRUE;
+}
+
+
+if(empty($_POST['gender'])){
+    print('Укажите пол.<br/>');
+    $errors=TRUE;
+}
+
+switch($_POST['gender']) {
+    case 'm': {
+        $gender='m';
+        break;
+    }
+    case 'f':{
+        $gender='f';
+        break;
+    }
+};
+
+if (empty($_POST['limbs'])) {
+    print('Укажите количество конечностей.<br/>');
+    $errors = TRUE;
+}
+
+switch($_POST['limbs']) {
+    case '2': {
+        $limbs='2';
+        break;
+    }
+    case '4':{
+        $limbs='4';
+        break;
+    }
+    case '8':{
+        $limbs='8';
+        break;
+    }
+    case '16':{
+        $limbs='16';
+        break;
+    }
+};
+
+if (empty($_POST['capabilities'])) {
+    print('Укажите хоть одну суперспособность.<br/>');
+    $errors = TRUE;
+}
+$power1=in_array('s1',$_POST['capabilities']) ? '1' : '0';
+$power2=in_array('s2',$_POST['capabilities']) ? '1' : '0';
+$power3=in_array('s3',$_POST['capabilities']) ? '1' : '0';
+$power3=in_array('s4',$_POST['capabilities']) ? '1' : '0';
+if (empty($_POST['agree'])) {
+    print('Вы не согласились с условиями контракта!<br/>');
+    $errors = TRUE;
+}
+$agree = 'agree';
+
+if ($errors) {
+    exit();
+}
+$user='u46878';
+$pass='2251704';
+$bd = new PDO('mysql:host=localhost;dbname=u46878',$user,$pass,array(PDO::ATTR_PERSISTENT => true));
+
+try{
+    $stmt = $bd->prepare("INSERT INTO application SET names = ?,email=?,bio=?,dateofbirth =?,gender=?,limbs=?,s1=?,s2=?,s3=?,s4=?");
+    $stmt = execute(array($_POST['names'],$_POST['email'],$_POST['bio'],$_POST['dayofbirth'],$gender,$limbs,$power1,$power2,$power3,$power4));
+}
+catch(PDOException $e){
+    print('Error : ' . $e->getMessage());
+    exit();
+}
+header('Location: ?save=1');
+?>
